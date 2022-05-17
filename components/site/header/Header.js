a2616ca8785d30be646498bdc0f077bdaca7ccba
simonjcarr <simonjcarr@gmail.com>
2022-05-17 18:36:49 +0100

    create HeaderMenu component and place in Header

--- components/site/header/Header.js
+++ components/site/header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import AuthMenu from '../../auth/AuthMenu'
+import HeaderMenu from './HeaderMenu'
 export default function Header() {
   return (
     <div className='site-header'>
       <div className="site-logo">Header</div>
-      <AuthMenu />
+      <div className="header-menus">
+        <HeaderMenu />
+        <AuthMenu />
+      </div>
     </div>
   )
 }
