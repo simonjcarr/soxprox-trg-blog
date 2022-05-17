904da769032d006ec3d30335fbf16aca27fdeb8e
simonjcarr <simonjcarr@gmail.com>
2022-05-17 14:19:23 +0100

    Added login logout buttons to header

---
+++ components/auth/AuthMenu.js
@@ -0,0 +1,17 @@
+import { AuthContext } from "../../stores/authContext";
+import { useContext } from 'react'
+
+export default function AuthMenu() {
+  const { user, login, logout, authReady } = useContext(AuthContext)
+  return (
+    <div className="auth-menu">
+      { authReady &&
+        <ul>
+          { !user && <li onClick={login}>Register / Login</li> }
+          { user && <li>{user.email}</li> }
+          { user && <li onClick={logout}>Logout</li> }
+        </ul>
+      }
+    </div>
+  )
+}
